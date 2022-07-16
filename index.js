abe635c02691ee813117e270fc8cd102bc0e3adf
unknown <tihanyiadam08@gmail.com>
2022-07-16 10:52:36 +0200

    kép megy elméletileg

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +20,16 @@ client.on('ready', () => {
 client.on('messageCreate', (message) => {
     var msg = message.content
     if(msg == "test") message.reply("done")
+})
+
+// when member added/joined
+const wcid = "934771375523115009"
+client.on('guildMemberAdd', async (member) =>{
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(wcid).send({
+        content: `Welcome <@${member.id}>!`,
+        files: [img]
+    })
 })
 
 // login / start
